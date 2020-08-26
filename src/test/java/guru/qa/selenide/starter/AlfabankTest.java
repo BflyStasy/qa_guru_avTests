c2f629aad7fe02251b673c7a03ccf65028a236cc
Olga Pankova <opankova@fil-it.ru>
2020-08-26 21:58:48 +0300

    tests done

--- src/test/java/guru/qa/selenide/starter/AlfabankTest.java
+++ src/test/java/guru/qa/selenide/starter/AlfabankTest.java
@@ -1,5 +1,9 @@
 package guru.qa.selenide.starter;
 
+import com.codeborne.selenide.CollectionCondition;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -8,15 +12,43 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AlfabankTest {
-  @Test
-  void testDeposit(){
+
+  @BeforeEach
+  void alfaInit(){
     open("http://alfabank.ru");
 
+  }
+
+  @AfterEach
+  void alfaClose() {
+    clearBrowserCookies();
+  }
+
+  @Test
+  void testDeposit(){
+    // Открываем депозиты
     $("body").shouldHave(text("Частным лицам"));
     $(byText("Вклады")).click();
     $("body").shouldHave(text("Вклады и инвестиции"));
     $$(byText("Депозиты")).find(visible).click();
-    $(".product-cell__row_recharged:not(.disabled)").$(".product-cell__cell-header").sibling(0).preceding(0).shouldHave(text("Победа"));
 
+    // клик на ссылку Архивные депозиты
+    $(".col-sm-8 a").click();
+
+    //Проверка количества элементов
+    $$(".product-cell__cell").shouldBe(CollectionCondition.size(3));
+  }
+
+  @Test
+  void alfaInsurance(){
+  // Открываем депозиты
+  $("body").shouldHave(text("Частным лицам"));
+  $(byText("Вклады")).click();
+
+  // Переходим на страхование вкладов
+  $(".navigation li").sibling(4).click();
+
+  // Проверяем, что заголовок "Страхование вкладов"
+  $(".breadcrumbs").sibling(0).shouldHave(text("Страхование вкладов"));
   }
 }
